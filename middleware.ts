22dafd29e6698b59be005e058dab745a815b5224
Francisco Moretti <franciscoemoretti@gmail.com>
2025-07-01 21:16:41 +0100

    Refactor middleware matcher and remove unused images
    - Updated the matcher in `middleware.ts` to exclude specific static files and images for better routing efficiency.
    - Deleted unused OpenGraph and Twitter images from the project to clean up assets.

--- middleware.ts
+++ middleware.ts
@@ -5,5 +5,7 @@ import { authConfig } from '@/app/(auth)/auth.config';
 export default NextAuth(authConfig).auth;
 
 export const config = {
-  matcher: ['/', '/:id', '/api/:path*', '/login', '/register'],
+  matcher: [
+    '/((?!_next/static|_next/image|favicon.ico|opengraph-image|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+  ],
 };

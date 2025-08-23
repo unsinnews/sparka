81cb157a81c061530c7def8c6b7a7b873f152104
Francisco Moretti <franciscoemoretti@gmail.com>
2025-08-23 21:00:44 +0100

    fix ?

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,7 @@ export const metadata: Metadata = {
 
 export const viewport = {
   maximumScale: 1, // Disable auto-zoom on mobile Safari
+  interactiveWidget: 'resizes-content' as const,
 };
 
 const geist = Geist({
